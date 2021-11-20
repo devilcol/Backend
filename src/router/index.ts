400e80ca5cd93e3f21f0cd92360ff3347df73f5c
devilcol <ltd160797@gmail.com>
2021-11-20 13:42:52 +0700

    controller build

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 
-const detailRouter = require('./detail')
+const detailRouter = require('./detailRouter')
 const homeController =require('../App/controllers/HomeController')
 
 const route = (app) => {
-    app.get("/", homeController.index )
     app.use('/:id', detailRouter)
+    app.get("/", homeController.index )
 }
 
 module.exports = route
